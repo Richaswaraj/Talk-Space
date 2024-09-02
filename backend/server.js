c179046c99a12a7ffbf49b9a46950f4786577123
richa swaraj <richaswaraj343@gmail.com>
2024-09-03 01:32:47 +0530

    3rd part completed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+//import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
@@ -5,12 +6,12 @@ import authRoutes from "./routes/authroutes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/users.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app,server} from "./socket/socket.js";
 
-const app = express();
-//const PORT = process.env.PORT || 3001;
 
 dotenv.config();
-const PORT = process.env.PORT || 6003;
+//const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,13 +19,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-//app.get("/", (req,res) => {
-//    res.send("Hello World");
+
+//app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+//app.get("*", (req, res) => {
+//	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 //});
 
-//app.use("/api/auth", authRoutes)
-
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
 });
